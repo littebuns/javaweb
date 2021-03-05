fa10c56faf2c871b98d7098d281dd17fb8972c96
xuxinbo <769289019@qq.com>
2021-03-05 16:54:52 +0800

    cookie 和 session 的基础用法

--- servlet/src/main/java/com/xxb/servlet/HelloServlet.java
+++ servlet/src/main/java/com/xxb/servlet/HelloServlet.java
@@ -2,11 +2,13 @@ package com.xxb.servlet;
 
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URLDecoder;
 
 /**
  * @author HP
@@ -15,8 +17,20 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setCharacterEncoding("utf-8");
         PrintWriter out = resp.getWriter();
-        out.write("Hello Servlet");
+        String user = (String) req.getSession().getAttribute("user");
+        String time = "";
+        Cookie[] cookies = req.getCookies();
+        for (Cookie cookie : cookies) {
+            if ("lastLoginTime".equals(cookie.getName())){
+                time = cookie.getValue();
+                break;
+            }
+        }
+        out.write("Hello " + user + "!"+ "\n");
+        out.write( "上次的登录时间为：" + URLDecoder.decode(time, "utf-8"));
     }
 
     @Override
--- servlet/src/main/java/com/xxb/servlet/LoginServlet.java
+++ servlet/src/main/java/com/xxb/servlet/LoginServlet.java
@@ -2,10 +2,13 @@ package com.xxb.servlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,8 +25,16 @@ public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getParameter("username");
         String pwd = req.getParameter("password");
-        if ("admin".equals(username)){
-            req.getRequestDispatcher("/hello").forward(req, resp);
+        if (username != null && userMap.get(username).equals(pwd)){
+            //登录成功，把用户名放入seesion中
+            req.getSession().setAttribute("user", username);
+            //将登录时间保存在cookie
+            Date date = new Date();
+            Cookie cookie = new Cookie("lastLoginTime", URLEncoder.encode(date.toString(), "utf-8"));
+            resp.addCookie(cookie);
+            resp.sendRedirect("/hello");
+        }else {
+            resp.sendError(HttpServletResponse.SC_FORBIDDEN);
         }
     }
 
